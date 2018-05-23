87c65651a3e19c40dc022a2fb90ea184c56532d7
Guillaume-Quinquet <guillaume.quinquet@hotmail.fr>
2018-05-23 11:52:45 +0200

    Initialization and upgrade to ES6

--- js/app.js
+++ js/app.js
@@ -1,32 +1,80 @@
+const allEnemies = [];
+
+class Entities {
+  constructor(x = 0,y = 0) {
+    this.origin
+    this.destination
+    this.x = x;
+    this.y = y;
+    this.sprite;
+  }
+}
+
 // Enemies our player must avoid
-var Enemy = function() {
+class Enemy extends Entities {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    constructor(x,y) {
+      super(x,y);
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+  }
+  // Update the enemy's position, required method for game
+  // Parameter: dt, a time delta between ticks
+  update(dt) {
+      // You should multiply any movement by the dt parameter
+      // which will ensure the game runs at the same speed for
+      // all computers.
+  };
+  // Draw the enemy on the screen, required method for game
+  render() {
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  };
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+class Player extends Entities {
+  constructor(x,y) {
+    super(x,y);
+    this.x = 200;
+    this.y = 380;
+    this.sprite = 'images/char-boy.png';
+  }
+  update(){
+
+  }
+
+  render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+  }
 
+  handleInput() {
+
+  }
+
+}
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
 
 // Now instantiate your objects.
+const enemy1 = new Enemy();
+const enemy2 = new Enemy();
+const enemy3 = new Enemy();
+const enemy4 = new Enemy();
+const enemy5 = new Enemy();
+const enemy6 = new Enemy();
+
+allEnemies.push(enemy1);
+allEnemies.push(enemy2);
+allEnemies.push(enemy3);
+allEnemies.push(enemy4);
+allEnemies.push(enemy5);
+allEnemies.push(enemy6);
+
+const player = new Player(1,1);
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
